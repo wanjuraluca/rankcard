58d0646a9575e6418d1e61ce5cf93b38da3cfb8c
Luca Wanjura <wanjuraluca@gmail.com>
2026-06-18 14:58:28 +0200

    feat: add tab bar with active state to profile page

--- app/components/ProfileClient.jsx
+++ app/components/ProfileClient.jsx
@@ -1,8 +1,11 @@
 "use client"
 
 import AvatarUpload from "./AvatarUpload"
+import { useState, useRef } from "react"
 
 export default function ProfileClient({ data, accounts }) {
+
+    const [activeTab, setActiveTab] = useState("overall")
 
     return(
         <div className="bg-background min-h-screen p-3">
@@ -17,11 +20,17 @@ export default function ProfileClient({ data, accounts }) {
                 <p className="text-text-primary text-lg font-bold">{data.username}</p>
                 <p className="text-text-secondary">{data.bio}</p>
                 <div className="flex gap-2 flex-wrap"> {/* Tags für Später m erken */}
-                    <span className="test-xs px-2 py-1 rounded-full bg-accent/10 text-accent border border-line">Member</span>
-                    <span className="test-xs px-2 py-1 rounded-full bg-accent/10 text-accent border border-line">Member</span>
+                    <span className="text-xs px-2 py-1 rounded-full bg-accent/10 text-accent border border-line">Member</span>
+                    <span className="text-xs px-2 py-1 rounded-full bg-accent/10 text-accent border border-line">Member</span>
                 </div>
                 </div>
             </div>
+            <div className="flex gap-2 mt-2 text-lg">
+                    <button onClick={() => setActiveTab("overall")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "overall" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>Overall Performance</button>
+                    <button onClick={() => setActiveTab("league")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "league" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>LoL</button>
+                    <button onClick={() => setActiveTab("valorant")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "valorant" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>Valorant</button>
+                    <button onClick={() => setActiveTab("addGame")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "addGame" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>+ Add Game</button>
+                </div>
         </div>
     )
 }
