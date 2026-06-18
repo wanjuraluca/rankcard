a7b3d60813445e9f07a292046695b3e2aadf79a9
Luca Wanjura <wanjuraluca@gmail.com>
2026-06-18 18:40:13 +0200

    feat: add overall tab with metric cars and sections headers

--- app/components/ProfileClient.jsx
+++ app/components/ProfileClient.jsx
@@ -13,7 +13,7 @@ export default function ProfileClient({ data, accounts }) {
             </div>
 
             <div className="bg-surface border border-line rounded-b-xl p-4 flex gap-4 items-center">
-                <div className="-mt-15">
+                <div className="-mt-25">
                     <AvatarUpload username={data.username} avatarUrl={data.avatar_url} />
                 </div>
                 <div>
@@ -25,12 +25,46 @@ export default function ProfileClient({ data, accounts }) {
                 </div>
                 </div>
             </div>
-            <div className="flex gap-2 mt-2 text-lg">
+            <div className="flex gap-2 mt-2">
                     <button onClick={() => setActiveTab("overall")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "overall" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>Overall Performance</button>
                     <button onClick={() => setActiveTab("league")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "league" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>LoL</button>
                     <button onClick={() => setActiveTab("valorant")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "valorant" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>Valorant</button>
                     <button onClick={() => setActiveTab("addGame")} className={`border rounded-lg bg-surface border-xl px-4 py-2 ${activeTab === "addGame" ? "border-accent bg-accent/10 text-accent" : "border-line"}`}>+ Add Game</button>
                 </div>
+                {activeTab === "overall" && (
+                    <div>
+                    <div className="flex items-center gap-2 mt-4 mb-2">
+                            <p className="text-text-secondary text-xs uppercase tracking-widest">Overall Performance</p>
+                            <div className="flex-1 h-px bg-line"></div>
+                        </div>
+                    <div className="grid grid-cols-2 gap-2 mt-2">
+                        <div className="bg-surface border border-line rounded-xl p-3">
+                            <p className="text-accent text-xl font-bold">2000</p>
+                            <p className="text-text-secondary text-xs">Rank Score</p>
+                        </div>
+                    <div className="bg-surface border border-line rounded-xl p-3">
+                            <p className="text-text-primary text-xl font-bold">55%</p>
+                            <p className="text-text-secondary text-xs">Avg. Win Rate</p>
+                    </div>
+                    <div className="bg-surface border border-line rounded-xl p-3">
+                            <p className="text-text-primary text-xl font-bold">2.47</p>
+                            <p className="text-text-secondary text-xs">Avg. KDA</p>
+                    </div>
+                    <div className="bg-surface border border-line rounded-xl p-3">
+                            <p className="text-text-primary text-xl font-bold">{accounts.length}</p>
+                            <p className="text-text-secondary text-xs">Games Connected</p>
+                        </div>
+                        <div className="flex items-center gap-2 mt-4 mb-">
+                            <p className="text-text-secondary text-xs uppercase tracking-widest">connected games</p>
+                            <div className="flex-1 h-px bg-line"></div>
+                            </div>
+                        </div>
+                        </div>
+                    )}
+                {activeTab === "league" && <div> LoL Inhalt</div>}
+                {activeTab === "valorant" && <div> Valorant Inhalt</div>}
+                {activeTab === "addGame" && <div> Add Game</div>}
+                
         </div>
     )
 }
